5310a7e05524bee4027ff28f32879543d7773639
Tom Burdick <tom.burdick@electromatic.us>
2021-09-24 12:54:07 -0500

    rddrone_fmuk66: pinmux settings for busses
    Adds pinmux settings for spi/i2c/uart/pwm
    
    Signed-off-by: Tom Burdick <tom.burdick@electromatic.us>

--- boards/arm/rddrone_fmuk66/pinmux.c
+++ boards/arm/rddrone_fmuk66/pinmux.c
@@ -41,19 +41,111 @@ static int rddrone_fmuk66_pinmux_init(const struct device *dev)
 
 
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(lpuart0), okay) && CONFIG_SERIAL
-	/* UART0 RX, TX */
+	/* LPUART0 RX, TX */
 	pinmux_pin_set(portd,  8, PORT_PCR_MUX(kPORT_MuxAlt5));
 	pinmux_pin_set(portd,  9, PORT_PCR_MUX(kPORT_MuxAlt5));
 #endif
 
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart0), okay) && CONFIG_SERIAL
+	/* UART0 RX, TX */
+	pinmux_pin_set(porta,  1, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(porta,  2, PORT_PCR_MUX(kPORT_MuxAlt2));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart1), okay) && CONFIG_SERIAL
+	/* UART1 RX, TX */
+	pinmux_pin_set(portc,  3, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portc,  4, PORT_PCR_MUX(kPORT_MuxAlt3));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart2), okay) && CONFIG_SERIAL
+	/* UART1 RX, TX */
+	pinmux_pin_set(portd,  2, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portd,  3, PORT_PCR_MUX(kPORT_MuxAlt3));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart4), okay) && CONFIG_SERIAL
+	/* UART1 RTS, CTS, RX, TX */
+	pinmux_pin_set(porte, 27, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portc, 13, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portc, 14, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portc, 15, PORT_PCR_MUX(kPORT_MuxAlt3));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(flexcan0), okay) && CONFIG_CAN
+	/* CAN0 TX, RX */
+	pinmux_pin_set(portb, 18, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portb, 19, PORT_PCR_MUX(kPORT_MuxAlt2));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(flexcan1), okay) && CONFIG_CAN
+	/* CAN1 TX, RX */
+	pinmux_pin_set(portc, 17, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc, 16, PORT_PCR_MUX(kPORT_MuxAlt2));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c0), okay) && CONFIG_I2C
+	/* I2C0 SCL, SDA */
+	pinmux_pin_set(porte, 24, PORT_PCR_MUX(kPORT_MuxAlt5));
+	pinmux_pin_set(porte, 25, PORT_PCR_MUX(kPORT_MuxAlt5));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c1), okay) && CONFIG_I2C
+	/* I2C0 SCL, SDA */
+	pinmux_pin_set(portc, 10, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc, 11, PORT_PCR_MUX(kPORT_MuxAlt2));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(spi0), okay) && CONFIG_SPI
+	/* SPI0 CS2, SCK, SIN, SOUT */
+	pinmux_pin_set(portc, 2, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc, 5, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc, 6, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc, 7, PORT_PCR_MUX(kPORT_MuxAlt2));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(spi1), okay) && CONFIG_SPI
+	/* SPI1 CS0, CS1, SCK, SIN, SOUT */
+	pinmux_pin_set(portb, 10, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portb,  9, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portb, 11, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portb, 16, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portb, 17, PORT_PCR_MUX(kPORT_MuxAlt2));
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(spi2), okay) && CONFIG_SPI
+	/* SPI2 CS0, SCK, SIN, SOUT */
+	pinmux_pin_set(portb, 20, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portb, 21, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portb, 22, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portb, 23, PORT_PCR_MUX(kPORT_MuxAlt2));
+#endif
+
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(ftm0), nxp_kinetis_ftm_pwm, okay) && CONFIG_PWM
+	/* FlexTimer0 Channels for FMU (servo control) */
+	/* ch1 */
+	pinmux_pin_set(portc,  1, PORT_PCR_MUX(kPORT_MuxAlt4));
+	/* ch2 */
+	pinmux_pin_set(porta,  4, PORT_PCR_MUX(kPORT_MuxAlt3));
+	/* ch3 */
+	pinmux_pin_set(portd,  4, PORT_PCR_MUX(kPORT_MuxAlt4));
+	/* ch 4 */ 
+	pinmux_pin_set(portd,  5, PORT_PCR_MUX(kPORT_MuxAlt4));
+#endif
+
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(ftm3), nxp_kinetis_ftm_pwm, okay) && CONFIG_PWM
-	/* FlexTimer Channels for PWM controlled RGB light */
+	/* FlexTimer3 Channels for PWM controlled RGB light and FMU (servo control) */
 	/* red */
 	pinmux_pin_set(portd,  1, PORT_PCR_MUX(kPORT_MuxAlt4));
 	/* green */
 	pinmux_pin_set(portc,  9, PORT_PCR_MUX(kPORT_MuxAlt3));
 	/* blue */
 	pinmux_pin_set(portc,  8, PORT_PCR_MUX(kPORT_MuxAlt3));
+	/* fmu ch 5 */ 
+	pinmux_pin_set(porte, 11, PORT_PCR_MUX(kPORT_MuxAlt6));
+	/* fmu ch 6 */
+	pinmux_pin_set(porte, 12, PORT_PCR_MUX(kPORT_MuxAlt6));
 #endif
 
 	return 0;
